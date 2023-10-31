30485341d0152bb770f3a50426af9cdbc2d5e772
Seng <2307@generative.xyz>
2023-10-31 19:23:09 +0700

    add assets and layout

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="h1-bold">the sang</h1>
-      <h2 className="h2-bold">the sang</h2>
-      <h3 className="h3-bold">the sang</h3>
-    </main>
-  );
-}
