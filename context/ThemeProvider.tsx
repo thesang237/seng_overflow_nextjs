1a507dd6323b21338b9d91e0446c273cdd457109
Seng <2307@generative.xyz>
2023-11-02 20:11:10 +0700

    update nav and fix theme config

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -11,20 +11,25 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
-
+  console.log("meomeo:", localStorage.theme);
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     handleThemeChange();
   }, [mode]);
+  console.log("MODE: ", mode);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
@@ -39,4 +44,5 @@ export function useTheme() {
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
+  return context;
 }
